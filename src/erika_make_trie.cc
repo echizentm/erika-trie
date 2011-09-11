5fe5f6300a3f64f927eb0e9c7ac8edac39cfe49d
echizentm <echizentm@580de0dd-1041-89a6-ccc1-31dbec538d6c>
2011-09-11 13:04:11 +0000

    add erika_make_trie.cc

---
+++ src/erika_make_trie.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "erika_trie.h"
+using namespace std;
+using namespace erika;
+
+int main(int argc, char **argv) {
+  if (argc < 2) {
+    cerr << "USAGE: erika_make_trie <outfile> < infile";
+    return 0;
+  }
+
+  try {
+    trie t;
+    uint   label;
+    ullong degree;
+    while (cin >> label) {
+      if (cin >> degree) {
+        t.push(label, degree);
+      }
+    }
+    t.write(argv[1]);
+  } catch (const char *err) {
+    cerr << err << endl;
+    return 1;
+  }
+  return 0;
+}
+
